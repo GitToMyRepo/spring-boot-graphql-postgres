2c1d8a633f5ff2ea03640f7f5aba168ee01ab92e
ken_git <kendontcare@gmail.com>
2026-04-02 23:10:04 +0100

    rename the test package, and add first junit with mockito

--- src/test/java/com/mywork/springgraphql/postgres/SpringBootGraphqlPostgresApplicationTests.java
+++ src/test/java/com/mywork/springgraphql/postgres/SpringBootGraphqlPostgresApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bezkoder.springgraphql.mysql;
+package com.mywork.springgraphql.postgres;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
